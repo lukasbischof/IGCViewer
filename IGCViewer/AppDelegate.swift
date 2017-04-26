5b2ac53df1f8276cfe702cc22be67ca671418fed
Lukas Bischof <polinderis@gmail.com>
2017-04-26 14:06:04 +0200

    Initial Commit

---
+++ IGCViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IGCViewer
+//
+//  Created by Lukas Bischof on 26.04.17.
+//  Copyright © 2017 Lukas Bischof. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IGCViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IGCViewer
+//
+//  Created by Lukas Bischof on 26.04.17.
+//  Copyright © 2017 Lukas Bischof. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
